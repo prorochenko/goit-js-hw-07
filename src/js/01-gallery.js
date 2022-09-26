eb3604e674062db3e77875c5d6ece4572c91de1a
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-09-26 21:47:19 +0300

    first task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,67 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+// 1) Создание и рендер разметки по массиву данных galleryItems и
+// предоставленному шаблону элемента галереи.
+
+//получаем ссылку на галерею
+const gallery = document.querySelector('.gallery');
+
+//делаем массив разметки галерии
+function addGalleryItems(items) {
+  return items
+    .map(item => {
+      const { preview, original, description } = item;
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join(``);
+}
+
+//добавляем массив в HTML
+gallery.insertAdjacentHTML('afterbegin', addGalleryItems(galleryItems));
+
+// создаем модалку
+
+gallery.addEventListener(`click`, handleClock);
+
+//переменная для того, чтобы была в глобале
+let currentTarget = null;
+
+function handleClock(event) {
+  //чтобы не срабатывало, когда нажимаем на пустое место между картинками
+  if (event.target === event.currentTarget) {
+    return;
+  }
+  event.preventDefault();
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`,
+    {
+      //слушатель клавиатуры, чтобы только слушало когда модалка открыта
+      onShow: () => window.addEventListener(`keydown`, escapeClose),
+      // убираем когда закрываем модалку
+      onclose: () => window.removeEventListener(`keydown`, escapeClose),
+    }
+  );
+
+  currentTarget = instance;
+
+  instance.show();
+}
+
+//закрыть модалку с помощью Esc
+function escapeClose(event) {
+  if (event.code === 'Escape') {
+    currentTarget.close();
+  }
+}
