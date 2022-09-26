d78ef95bda0ed8be6136e7ad32318226445143ac
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-09-26 23:03:35 +0300

    Update 02-lightbox.js

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -23,41 +23,7 @@ const markup = galleryItems
 //добавляем массив в HTML
 gallery.insertAdjacentHTML('afterbegin', markup);
 
-new SimpleLightbox('.gallery a', {
+//добавляем SimpleLightbox
+new SimpleLightbox('.gallery__link', {
   /* options */
 });
-
-// создаем модалку
-
-gallery.addEventListener(`click`, handleClock);
-
-//переменная для того, чтобы была в глобале
-let currentTarget = null;
-
-function handleClock(event) {
-  //чтобы не срабатывало, когда нажимаем на пустое место между картинками
-  if (event.target === event.currentTarget) {
-    return;
-  }
-  event.preventDefault();
-  const instance = basicLightbox.create(
-    `<img src="${event.target.dataset.source}" width="800" height="600">`,
-    {
-      //слушатель клавиатуры, чтобы только слушало когда модалка открыта
-      onShow: () => window.addEventListener(`keydown`, escapeClose),
-      // убираем когда закрываем модалку
-      onclose: () => window.removeEventListener(`keydown`, escapeClose),
-    }
-  );
-
-  currentTarget = instance;
-
-  instance.show();
-}
-
-//закрыть модалку с помощью Esc
-function escapeClose(event) {
-  if (event.code === 'Escape') {
-    currentTarget.close();
-  }
-}
